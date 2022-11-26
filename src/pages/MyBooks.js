31d09fd1183f6c108e72c7bf08d34623159b16eb
luis.poveda93 <luis.poveda9321@gmail.com>
2022-11-26 01:37:15 +0100

    debugging vercel

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -8,7 +8,7 @@ const MyBooks = () => {
     const { address, isConnected } = useAccount();
 
     const fetchNFT = async () => {
-        const res = await fetch(`https://api/get-my-books?address=${address}`, {
+        const res = await fetch(`/api/get-my-books?address=${address}`, {
             "method": "GET",
             }
         )
