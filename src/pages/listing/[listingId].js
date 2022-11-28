7851e83237656c857074c3e7ded19086cbef3564
luis.poveda93 <luis.poveda9321@gmail.com>
2022-11-28 23:21:38 +0100

    request updated

--- src/pages/listing/[listingId].js
+++ src/pages/listing/[listingId].js
@@ -45,7 +45,7 @@ const ListingPage = () => {
 
   const fetchCheckoutLink = async () => { // fetch the checkout link from the server
     setPending(true);
-    const response = await fetch(`https://obscure-taiga-79570.herokuapp.com/api/create-paper-intent?listingId=${listingId}&img=${img}`);
+    const response = await fetch(`https://obscure-taiga-79570.herokuapp.com/api/create-paper-intent?listingId=${listingId}&img=${img}&name=${nft.metadata.name}`);
     const data = await response.json();
     window.open(data.checkoutLinkIntentUrl, "_blank");
     setPending(false);
